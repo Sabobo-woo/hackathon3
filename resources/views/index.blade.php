91a88861e56aa7ba7634b78001079db3678a30ec
Sabina Tokadzhieva <jenskikomar@gmail.com>
2022-10-21 12:44:52 +0200

    working on homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to our cute animal database</h1>
+      <ul>
+    <?php foreach($animals as $animal) : ?>
+  
+    <li>
+       <?= $animal->name ?>
+       <ul>
+        <li>Breed: <?= $animal->breed ?></li>
+        <li>Owner Name: <?= $animal->owner->first_name?> <?= $animal->owner->surname?></li>
+        <li><img src="/images/pets/<?= $animal->image->path?>" alt = "<?= $animal->name ?>"></li></li>
+       </ul>
+    </li>
+    <?php endforeach; ?>
+    </ul>
+
+    <div style="width:10px; heigth:auto; display:flex; flex-direction:row">{{$animals->links()}}</div>
+</body>
+</html>
